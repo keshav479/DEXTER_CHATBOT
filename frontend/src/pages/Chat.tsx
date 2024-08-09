f257670f798cf4e2d33f1880192f6ee10ea68577
Your Name <you@example.com>
2024-08-09 21:04:54 +0530

    error removed

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,5 +1,5 @@
 import { Box, Avatar, Typography, Button, IconButton } from '@mui/material'
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { useAuth } from '../context/AuthContext';
 import { red } from '@mui/material/colors';
 import ChatItem from '../components/chat/ChatItem';
