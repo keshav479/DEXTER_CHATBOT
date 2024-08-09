a8c1c4f20caba3c301eddbdd79135e1b3a06f024
Your Name <you@example.com>
2024-08-09 22:18:22 +0530

    change

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,13 +2,11 @@ import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 import * as dotenv from 'dotenv';
 dotenv.config();
-const PORT = process.env.PORT || 3008;
-
-
+const PORT = process.env.PORT || 8080;
 connectToDatabase().then(() => {
     // app.get("/hello",(req,res,next)=>{
     //     return res.send("HEYYY");
     // })
-    app.listen(3008, () => console.log("Server open and conected to database 👍"));
+    app.listen(8080, () => console.log("Server open and conected to database 👍"));
 }).catch(err => console.log(err));
 //# sourceMappingURL=index.js.map
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_js_1 = require("./app.js");
 var connection_js_1 = require("./db/connection.js");
-var PORT = process.env.PORT || 3004;
+var PORT = process.env.PORT || 8080;
 (0, connection_js_1.connectToDatabase)().then(function () {
     // app.get("/hello",(req,res,next)=>{
     //     return res.send("HEYYY");
     // })
-    app_js_1.default.listen(3008, function () { return console.log("Server open and conected to database 👍"); });
+    app_js_1.default.listen(8080, function () { return console.log("Server open and conected to database 👍"); });
 }).catch(function (err) { return console.log(err); });
