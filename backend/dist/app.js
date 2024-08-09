1fe85833af1a99b5570b3bff95852b8dba006b5c
Your Name <you@example.com>
2024-08-09 21:12:55 +0530

    updated

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -9,7 +9,7 @@ const app = express();
 //middleware
 app.use(cors({ origin: "https://dexter-chatbot.vercel.app", credentials: true }));
 // app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 //remove it in production
 app.use(morgan("dev"));
