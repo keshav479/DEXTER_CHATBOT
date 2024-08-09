c2737c3a2a8021655cf232f64c679721c5376c46
Your Name <you@example.com>
2024-08-09 21:47:06 +0530

    port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,11 @@ import { connectToDatabase } from "./db/connection.js"
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.PORT || 3008;
+const PORT = process.env.PORT || 8080;
 connectToDatabase().then(() => {
 // app.get("/hello",(req,res,next)=>{
 //     return res.send("HEYYY");
 // })
-app.listen(3008, () => console.log("Server open and conected to database 👍"));
+app.listen(8080, () => console.log("Server open and conected to database 👍"));
 
 }).catch(err => console.log(err));
